77d1e496857725786942f77ff412c4cd4490bc93
aaqib123 <aaqib.hyd@gmail.com>
2016-06-09 20:19:48 -0300

    generates names and links

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,26 @@
+"""Script to gather IMDB keywords from 2013's top grossing movies."""
+import sys
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+URL = "http://www.imdb.com/search/title?at=0&sort=boxoffice_gross_us,desc&start=1&year=2013,2013"
+
+def get_top_grossing_movie_links(url):
+    """Return a list of tuples containing the top grossing movies of 2013 and link to their IMDB
+    page."""
+    response = requests.get(url)
+    movies_list = []
+    print (response)
+    for each_url in BeautifulSoup(response.text).select('.title a[href*="title"]'):
+        movie_title = each_url.text 
+        if movie_title != 'X':
+            movies_list.append((movie_title, each_url['href']))
+    return movies_list
+
+
+movies = get_top_grossing_movie_links(URL)
+with open('output.csv', 'w') as output:
+    csvwriter = csv.writer(output)
+    for title, url in movies:         
+        csvwriter.writerow([title, "http://www.imdb.com"+ url])
